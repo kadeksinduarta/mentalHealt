0802d8bbceda0f5fba689d3ef09cee3ec2873dd7
kadeksinduarta <ikadeksinduarta@gmail.com>
2023-09-11 11:10:44 +0800

    up awal

---
+++ Script/script.js
@@ -0,0 +1,15 @@
+const navbarLink = document.querySelector(".navbar-link");
+
+// ketika menu hamberger di click
+document.querySelector("#menu").onclick = () => {
+  navbarLink.classList.toggle("active");
+};
+
+// click di luar sideber untuk menghilangkan nav nya
+const hambergerMenu = document.querySelector("#menu");
+
+document.addEventListener("click", function (e) {
+  if (!hambergerMenu.contains(e.target) && !navbarLink.contains(e.target)) {
+    navbarLink.classList.remove("active");
+  }
+});
